2fee2b5b1fab0ca81f83f6d1c02cb7b9affb29e3
baoxin <bx19920704@sina.com>
2019-02-22 14:18:07 +0800

    test commit
    Signed-off-by: baoxin <bx19920704@sina.com>

--- platform-api/src/main/java/com/platform/annotation/IgnoreAuth.java
+++ platform-api/src/main/java/com/platform/annotation/IgnoreAuth.java
@@ -7,7 +7,7 @@ import java.lang.annotation.*;
  *
  * @author lipengjun
  * @email 939961241@qq.com
- * @date 2017-03-23 15:44
+ * @date 2017-03-23 15:44 
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
